bf3efcf390584899539d9c2edec4ef9e26c0f709
Anton Deguet <anton.deguet@jhu.edu>
2022-08-18 17:21:20 -0400

    * re-ordered pins to match order on revolute pots * use array of arrays for JSON output (primary vs secondary pots)

--- dessj_ble.ino
+++ dessj_ble.ino
@@ -5,10 +5,10 @@ BLEService sujservice("babb122c-de4c-11ec-9d64-0242ac120100");
 uint16_t all_pots_values[8] = {0};
 
 BLEStringCharacteristic all_pots_char("babb122c-de4c-11ec-9d64-0242ac120101",  // standard 16-bit characteristic UUID
-    BLERead | BLENotify, 128); // remote clients will be able to get notifications if this characteristic changes
+				      BLERead | BLENotify, 128); // remote clients will be able to get notifications if this characteristic changes
 
-uint8_t analog_pins[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
-char output_string[128] = "";
+uint8_t analog_pins[8] = {A7, A5, A3, A1, A6, A4, A2, A0};
+char output_string[256] = "";
 
 void setup() {
   Serial.begin(9600);
@@ -21,16 +21,15 @@ void setup() {
 
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
-
     while (1);
   }
 
   BLE.setDeviceName("dVRK SUJ");
   BLE.setLocalName("dVRK SUJ");
-  
+
   sujservice.addCharacteristic(all_pots_char);
   BLE.addService(sujservice);
-  BLE.setAdvertisedService(sujservice); 
+  BLE.setAdvertisedService(sujservice);
 
   BLE.advertise();
 
@@ -42,11 +41,11 @@ void loop() {
   for (int i = 0; i < 8; i++) {
     uint32_t sum = 0;
     for (int j = 0; j < 16 * 16; j++) {
-      sum += analogRead(analog_pins[i]);      
+      sum += analogRead(analog_pins[i]);
     }
     uint16_t averaged = sum / 16;
     all_pots_values[i] = averaged;
   }
-  sprintf(output_string, "{\"timestamp\":%U, \"pots\":[%u,%u,%u,%u,%u,%u,%u,%u]}", millis(), all_pots_values[0], all_pots_values[1], all_pots_values[2], all_pots_values[3], all_pots_values[4], all_pots_values[5], all_pots_values[6], all_pots_values[7]);
+  sprintf(output_string, "{\"timestamp\":%U, \"pots\":[[%u,%u,%u,%u],[%u,%u,%u,%u]]}", millis(), all_pots_values[0], all_pots_values[1], all_pots_values[2], all_pots_values[3], all_pots_values[4], all_pots_values[5], all_pots_values[6], all_pots_values[7]);
   all_pots_char.writeValue(output_string);
 }
